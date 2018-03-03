2fa7c2ca61e7e818c30d5025583d48f4523e8212
Yuhuan Jiang <jyuhuan@users.noreply.github.com>
2018-03-03 00:30:32 -0800

    Create do.py

---
+++ do.py
@@ -0,0 +1,23 @@
+def do_impl(gen, m, *args, **kwargs):
+    boxes = gen(*args, **kwargs)
+
+    def receive(box_content):
+        try:
+            box = boxes.send(box_content)
+        except StopIteration:
+            return m.id(None)
+        except Return as r:
+            return m.id(r.value)
+        else:
+            return box.flat_map(receive)
+
+    return receive(None)
+
+
+def do(m):
+    def wrapper(f):
+        def g(*args, **kwargs):
+            return do_impl(f, m, *args, **kwargs)
+        return g
+    return wrapper
+    
