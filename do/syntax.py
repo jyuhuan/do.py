dfa2ead0c87789af8de9643e7182c8ab080bce1d
Yuhuan Jiang <jyuhuan@gmail.com>
2018-03-06 01:33:57 -0800

    Support monads which calls f in flat_map multiple times

--- do/syntax.py
+++ do/syntax.py
@@ -1,8 +1,25 @@
 from do.exceptions import NoReturnException
 
 
-def _do_impl(gen, m, *args, **kwargs):
+def do(m):
+    def wrapper(f):
+        def g(*args, **kwargs):
+            return _do_impl(f, m(), *args, **kwargs)
+        return g
+    return wrapper
 
+
+def _do_impl(gen, m, *args, **kwargs):
+    """Runs the generator of monads.
+
+    This method takes into consideration the fact that the ``flat_map(m, f)``
+    method of some monads may use invoke the ``f`` argument multiple times (for
+    example, the list monad).
+
+    Ported from the ``Do.Multi`` of Fantasy Do [1].
+
+    [1] https://github.com/russellmcc/fantasydo
+    """
     try:
         boxes = gen(*args, **kwargs)
         if boxes is None:
@@ -10,8 +27,10 @@ def _do_impl(gen, m, *args, **kwargs):
     except Return as r:
         return m.id(r.value)
     else:
-
-        def receive(box_content):
+        def receive(box_content, state):
+            boxes = gen(*args, **kwargs)
+            for it in state:
+                boxes.send(it)
             try:
                 box = boxes.send(box_content)
             except StopIteration:
@@ -19,17 +38,9 @@ def _do_impl(gen, m, *args, **kwargs):
             except Return as r:
                 return m.id(r.value)
             else:
-                return m.flat_map(box, receive)
+                return m.flat_map(box, lambda x: receive(x, state + [box_content]))
 
-        return receive(None)
-
-
-def do(m):
-    def wrapper(f):
-        def g(*args, **kwargs):
-            return _do_impl(f, m(), *args, **kwargs)
-        return g
-    return wrapper
+        return receive(None, [])
 
 
 class Return(Exception):
